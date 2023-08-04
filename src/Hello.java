5c7b40dd01e3e85204b5a7d4284265409dd881dc
Shailesh Kumar Thakur <shaileshthakur94@gmail.com>
2023-08-04 16:10:24 +0530

    First Commit

---
+++ src/Hello.java
@@ -0,0 +1,31 @@
+public class Hello {
+
+    public static void main (String[] args) {
+
+        String name[] = new String[5];
+        float marks[] = new float[5];
+
+        int[] rolls = {10, 20, 30, 40, 50};
+
+        name[0]="pinki";
+        name[1]="priya";
+        name[2]="ansh";
+        name[3]="abhi";
+        name[4]="ajay";
+
+        marks[0] = 98;
+
+        for (int i = 0; i < rolls.length; i++) {
+
+            if(int mark=98)
+            {
+                System.out.println(name[0]);
+            }
+
+
+            System.out.println(rolls[i]);
+
+        }
+    }
+
+}
