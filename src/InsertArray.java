f455db944b39ea55440846a6a2bf2a6d0cc0dcf0
Shailesh Kumar Thakur <shaileshthakur94@gmail.com>
2023-08-05 10:32:32 +0530

    InsertLastelement

---
+++ src/InsertArray.java
@@ -0,0 +1,11 @@
+public class InsertArray {
+    public static void main(String args[]){
+        int numbers[]={10,20,30,40,50,60,70};
+        numbers[6]=80;
+
+        for(int i=0;i<numbers.length;i++){
+
+        }
+
+    }
+}
